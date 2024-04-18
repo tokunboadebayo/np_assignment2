d6a11576e2215d79607292def241147d259d66bf
Emmanuel Adebayo <adebayotokunbo@gmail.com>
2024-04-18 19:09:56 +0200

    Error and check for timeout using error number

--- clientmain.cpp
+++ clientmain.cpp
@@ -79,3 +79,31 @@ error_codes send_message(int socket_fd, void *message, uint32_t message_len, str
     }
     return OK;
 }
+
+error_codes receive_message(int socket_fd, void *message, uint32_t message_len, struct sockaddr_in *server_address) {
+    if (message == nullptr) {
+        return ERROR;
+    }
+    memset(message, 0, message_len);
+
+    if (server_address == nullptr) {
+        return ERROR;
+    }
+    memset(server_address, 0, sizeof(struct sockaddr_in));
+
+    ssize_t received_bytes;
+    socklen_t server_address_len = sizeof(struct sockaddr_in);
+    received_bytes = recvfrom(socket_fd, message, message_len, 0, (struct sockaddr *) server_address,
+                              &server_address_len);
+    if (received_bytes < 0) {
+        // check for the timeout using errno
+        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+            return TIMEOUT;
+        }
+        perror("recvfrom");
+        return ERROR;
+    }
+
+    return OK;
+}
+
