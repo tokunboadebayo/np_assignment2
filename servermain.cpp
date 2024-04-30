48935c0b032fa5e6d8340ff60043c96eeeb33d73
Emmanuel Adebayo <adebayotokunbo@gmail.com>
2024-04-30 06:51:16 +0200

    Enumerators for opertions

--- servermain.cpp
+++ servermain.cpp
@@ -3,11 +3,12 @@
 #include <string.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 #include <netdb.h>
 #include <time.h>
 #include <stdbool.h>
 #include "protocol.h"
-
+#include "calcLib.h"
 
 
 int get_server_address_info(const char *server_name, const char *Destport, struct sockaddr_storage *server_addr);
@@ -24,63 +25,32 @@ int create_server_socket(int port, struct sockaddr_storage *server_address);
 #define MAX_CLIENTS 100
 #define INVALID_CLIENT_ID 0
 
+// Enum for operation mapping
+enum operation {
+    ADD = 1,
+    SUB = 2,
+    MUL = 3,
+    DIV = 4,
+    FADD = 5,
+    FSUB = 6,
+    FMUL = 7,
+    FDIV = 8,
+};
 
-using namespace std;
-/* Needs to be global, to be rechable by callback and main */
-int loopCount=0;
-int terminate=0;
+enum client_state {
+    DISCONNECTED = 0,
+    CONNECTED = 1,
+    OPERATION_SENT = 2,
+    RESULT_RECEIVED = 3,
+    RESULT_SENT = 4,
+    TIMEOUT = 5
+};
 
+struct assignment {
+    enum operation operation;
+    float fval[2];
+    int ival[2];
+    double result;
+    bool is_float;
+};
 
-/* Call back function, will be called when the SIGALRM is raised when the timer expires. */
-void checkJobbList(int signum){
-  // As anybody can call the handler, its good coding to check the signal number that called it.
-
-  printf("Let me be, I want to sleep, loopCount = %d.\n", loopCount);
-
-  if(loopCount>20){
-    printf("I had enough.\n");
-    terminate=1;
-  }
-  
-  return;
-}
-
-
-
-
-
-int main(int argc, char *argv[]){
-  
-  /* Do more magic */
-
-
-  /* 
-     Prepare to setup a reoccurring event every 10s. If it_interval, or it_value is omitted, it will be a single alarm 10s after it has been set. 
-  */
-  struct itimerval alarmTime;
-  alarmTime.it_interval.tv_sec=10;
-  alarmTime.it_interval.tv_usec=10;
-  alarmTime.it_value.tv_sec=10;
-  alarmTime.it_value.tv_usec=10;
-
-  /* Regiter a callback function, associated with the SIGALRM signal, which will be raised when the alarm goes of */
-  signal(SIGALRM, checkJobbList);
-  setitimer(ITIMER_REAL,&alarmTime,NULL); // Start/register the alarm. 
-
-#ifdef DEBUG
-  printf("DEBUGGER LINE ");
-#endif
-  
-  
-  while(terminate==0){
-    printf("This is the main loop, %d time.\n",loopCount);
-    sleep(1);
-    loopCount++;
-  }
-
-  printf("done.\n");
-  return(0);
-
-
-  
-}
